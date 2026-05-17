b10589cc67731112a457699c2e10c5bc01eeae81
Chon211205 <lee241203@uvg.edu.gt>
2026-05-17 15:41:55 -0600

    password strength meter

--- src/components/PasswordStrengthMeter.jsx
+++ src/components/PasswordStrengthMeter.jsx
@@ -1,9 +1,23 @@
+import { useState } from 'react'
+import { getPasswordStrength } from '../utils/passwordStrength'
+
 export default function PasswordStrengthMeter () {
+  const [password, setPassword] = useState('')
+
+  const strength = getPasswordStrength(password)
+
   return (
     <section>
       <label htmlFor="password">Contraseña</label>
-      <input id="password" type="password" />
-      <p>vacía</p>
+
+      <input
+        id="password"
+        type="password"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+      />
+
+      <p aria-live="polite">{strength}</p>
     </section>
   )
 }
